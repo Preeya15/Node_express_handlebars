0410872637ea9d0673969ab364ad942824fd1664
Priya Sharma <priyasharma15@gmail.com>
2021-02-25 22:15:26 +1100

    adding files

---
+++ db/seed.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name, devoured ) VALUES ('Cheese Burger', 2);
+INSERT INTO burgers (burger_name, devoured ) VALUES ('Veg Burger', 3);
+INSERT INTO burgers (burger_name, devoured ) VALUES ('Snitzel Bruger', 1);
---
+++ models/burger.js
@@ -0,0 +1,28 @@
+const orm = require('../config/orm');
+
+// call the orm functions using specific imput
+const burger = {
+    all: function (cb) {
+        orm.all('burgers', function(res){
+            cb(res);
+        })
+    },
+    create: function (cols, vals, cb) {
+        orm.create('burgers', cols, vals, function(res) {
+            cb(res);
+        })
+    },
+    update: function (objColVals, condition, cb) {
+        orm.update('burgers', objColVals, condition, function(res){
+            cb(res);
+        })
+    },
+    delete: function (condition, cb) {
+        orm.delete('burgers', condition, function(res){
+            cb(res);
+        })
+    },
+
+}
+//export burger.js using module exports
+module.exports = burger
